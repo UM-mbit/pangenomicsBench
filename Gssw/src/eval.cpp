02945344f3b133276d3ca787d28144d5bb63e357
kaplannp <kaplannp@umich.edu>
2024-07-21 10:42:40 -0400

    modify to use the mapping print function from gssw

--- Gssw/src/eval.cpp
+++ Gssw/src/eval.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <cstdlib>
+#include <stdio.h>
 #include <fstream>
 #include "eval.h"
 #include "gssw.h"
@@ -7,11 +8,12 @@
 #include "gssw_to_json.hpp"
 
 void write_ggm(std::string out_dir, int ind, gssw_graph_mapping* ggm){
-  std::ofstream out_file(out_dir + "/mapping"+std::to_string(ind)+".json");
-  //write outputs to file
-  nlohmann::json ggm_json_serialized = dump_graph_mapping(ggm);
-  std::string ggm_json_string = ggm_json_serialized.dump(2);
-  out_file << ggm_json_string << std::endl;
+  std::string stager("");
+  stager = out_dir + "/mapping"+std::to_string(ind)+".json";
+  FILE* out_file;
+  out_file = fopen(stager.c_str(), "w");
+  gssw_print_graph_mapping(ggm, out_file);
+  fclose(out_file);
 }
 
 void init_output_dir(std::string output_dir){
