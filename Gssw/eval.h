c61f15d5df53c90c685f2ebcb29de534daef0735
kaplannp <kaplannp@umich.edu>
2024-07-21 09:52:48 -0400

    This passes with correct outputs

--- Gssw/eval.h
+++
@@ -1,8 +0,0 @@
-#ifndef __EVAL_H__
-#define __EVAL_H__
-#include "deps/Gssw/src/gssw.h"
-#include <string>
-
-void write_ggm(std::ofstream& out_file, gssw_graph_mapping* ggm);
-
-#endif //__EVAL_H__
