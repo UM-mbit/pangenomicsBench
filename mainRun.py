c8679cdea482980a61e48251a87a316a5d99f9b2
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2024-10-16 17:59:41 -0400

    cleanup of mainRun.py

--- mainRun.py
+++ mainRun.py
@@ -17,48 +17,6 @@ class PangenomicsBenchRunner(Runner):
         shutil.move("Out", os.path.join(self.kernelOutputDir, "Out"))
 
 
-# pgsgd runner
-# vanilla (6 threads)
-#outDir="out"
-#app="./pgsgd/bin/pgsgd 6"
-#execAndLog("rm -rf {}".format(outDir))
-#runner = Runner(outDir)
-#runner.runVanillaApp(app, outputs=["out_benchmark.lay"])
-#runner.printResults()
-
-# VtuneUarch
-#outDir="out"
-#app="./pgsgd/bin/pgsgd.prof"
-#execAndLog("rm -rf {}".format(outDir))
-#runner = Runner(outDir)
-#runner.runVtuneUarch(app)
-#runner.printResults()
-
-# VtuneCache
-#outDir="out"
-#app="./pgsgd/bin/pgsgd.prof"
-#execAndLog("rm -rf {}".format(outDir))
-#runner = Runner(outDir)
-#runner.runVtuneCache(app)
-#runner.printResults()
-
-# PinInstrCount
-#outDir="out"
-#app="./pgsgd/bin/pgsgd.prof 1"
-#execAndLog("rm -rf {}".format(outDir))
-#runner = Runner(outDir)
-#runner.runPinInstrCount(app)
-#runner.printResults()
-
-# Threadscaling
-#outDir="out"
-#app="./pgsgd/bin/pgsgd.prof {}"
-#execAndLog("rm -rf {}".format(outDir))
-#runner = Runner(outDir)
-#runner.runThreadScaling(app, [1, 8, 16, 32])
-#runner.printResults()
-
-
 # combined transclosure
 #outDir="out"
 #execAndLog("rm -rf {}".format(outDir))
@@ -74,18 +32,30 @@ class PangenomicsBenchRunner(Runner):
 #runner.runPinInstrCount(app)
 #runner.printResults()
 
-
-#outDir="Outs/CacheUBenchmark"
-#app="OMP_NUM_THREADS={} ./Dummy/bin/dummy.omp /data2/kaplannp/Genomics/Datasets/Kernels/Dummy"
-#app="./CacheUBenchmark/bin/cacheUBenchmark.prof"
-#outDir="Outs/CacheUBenchmark"
-#app="OMP_NUM_THREADS={} ./Dummy/bin/dummy.omp /data2/kaplannp/Genomics/Datasets/Kernels/Dummy"
-#app="./CacheUBenchmark/bin/cacheUBenchmark.prof"
+# combined pgsgd
+#outDir="out"
 #execAndLog("rm -rf {}".format(outDir))
 #runner = Runner(outDir)
+#app="./pgsgd/bin/pgsgd 8"
+#runner.runVanillaApp(app, outputs=["out_benchmark.lay"])
+#app="./pgsgd/bin/pgsgd.prof 8"
+#runner.runVtuneUarch(app)
+#runner.runVtuneCache(app)
+#app="./pgsgd/bin/pgsgd.prof {}"
+#runner.runThreadScaling(app, [1] + [4] + list(range(8, 65, 8)))
+#app="./pgsgd/bin/pgsgd.prof 1"
+#runner.runPinInstrCount(app)
+#runner.printResults()
+
+
+outDir="Outs/CacheUBenchmark"
+#app="OMP_NUM_THREADS={} ./Dummy/bin/dummy.omp /data2/kaplannp/Genomics/Datasets/Kernels/Dummy"
+app="./CacheUBenchmark/bin/cacheUBenchmark.prof"
+execAndLog("rm -rf {}".format(outDir))
+runner = Runner(outDir)
 #runner.runThreadScaling(app, [1] + list(range(8, 65, 8)))
 #runner.runVanillaApp(app, outputs="Out")
 #runner.runPinInstrCount(app)
 #runner.runVtuneUarch(app)
-#runner.runVtuneCache(app)
+runner.runVtuneCache(app)
 #runner.printResults()
