30678916deb98344146fbe15cc449a06c6ef21a3
kaplannp <kaplannp@umich.edu>
2025-04-07 06:45:34 -0400

    updates for new kernels

--- mainRun.py
+++ mainRun.py
@@ -26,7 +26,8 @@ Set these to true to run the corresponding analysis. Uarch and Cache run the
 application once each, so enabling both triples execution time.
 PIN can increase runtime by a larger factor.
 '''
-RUN_UARCH = False
+RUN_VANILLA = False
+RUN_UARCH = True
 RUN_CACHE = False
 RUN_PIN = False
 
@@ -43,38 +44,48 @@ os.system("mkdir AllRunsOut")
 #kernel time: 28096616us
 #write time: 549466us
 gsswRunner = Runner("AllRunsOut/Gssw")
-gsswRunner.runVanillaApp("{}/Gssw/bin/gssw.prof $KERNEL_DATA/Gssw/McFirst1e4Reads".format(cwd))
-if RUN_UARCH: gsswRunner.runVtuneUarch("{}/Gssw/bin/gssw.prof $KERNEL_DATA/Gssw/McFirst1e4Reads".format(cwd))
-if RUN_CACHE: gsswRunner.runVtuneCache("{}/Gssw/bin/gssw.prof $KERNEL_DATA/Gssw/McFirst1e4Reads".format(cwd))
-pinRuns.append(lambda:gsswRunner.runPinInstrCount("{}/Gssw/bin/gssw.prof $KERNEL_DATA/Gssw/McFirst1e3Reads".format(cwd)))
+if RUN_VANILLA : gsswRunner.runVanillaApp("{}/Gssw/bin/gssw.prof $KERNEL_DATA/Gssw/McSubsampledShortReads".format(cwd))
+if RUN_UARCH: gsswRunner.runVtuneUarch("{}/Gssw/bin/gssw.prof $KERNEL_DATA/Gssw/McSubsampledShortReads".format(cwd))
+if RUN_CACHE: gsswRunner.runVtuneCache("{}/Gssw/bin/gssw.prof $KERNEL_DATA/Gssw/McSubsampledShortReads".format(cwd))
+pinRuns.append(lambda:gsswRunner.runPinInstrCount("{}/Gssw/bin/gssw.prof $KERNEL_DATA/Gssw/McSubsampledShortReads".format(cwd)))
 
 #load time: 27095355us
 #kernel time: 23283850us
 #write time: 1846884us
 gbvRunner = Runner("AllRunsOut/Gbv")
-gbvRunner.runVanillaApp("{}/Gbv/bin/gbv.prof $KERNEL_DATA/Gbv/Mc1e3Reads 1000".format(cwd))
-if RUN_UARCH: gbvRunner.runVtuneUarch("{}/Gbv/bin/gbv.prof $KERNEL_DATA/Gbv/Mc1e3Reads 1000".format(cwd))
-if RUN_CACHE: gbvRunner.runVtuneCache("{}/Gbv/bin/gbv.prof $KERNEL_DATA/Gbv/Mc1e3Reads 1000".format(cwd))
-pinRuns.append(lambda:gbvRunner.runPinInstrCount("{}/Gbv/bin/gbv.prof $KERNEL_DATA/Gbv/Mc1e3Reads 100".format(cwd)))
+if RUN_VANILLA : gbvRunner.runVanillaApp("{}/Gbv/bin/gbv.prof $KERNEL_DATA/Gbv/McSubsampledLongReads".format(cwd))
+if RUN_UARCH: gbvRunner.runVtuneUarch("{}/Gbv/bin/gbv.prof $KERNEL_DATA/Gbv/McSubsampledLongReads".format(cwd))
+if RUN_CACHE: gbvRunner.runVtuneCache("{}/Gbv/bin/gbv.prof $KERNEL_DATA/Gbv/McSubsampledLongReads".format(cwd))
+pinRuns.append(lambda:gbvRunner.runPinInstrCount("{}/Gbv/bin/gbv.prof $KERNEL_DATA/Gbv/McSubsampledLongReads".format(cwd)))
 
 #load time: 292282215us
 #kernel time: 35296243us
 #write time: 6166688us
 gbwtRunner = Runner("AllRunsOut/Gbwt")
-gbwtRunner.runVanillaApp("{}/Gbwt/bin/gbwt.prof $KERNEL_DATA/Gbwt/McFullGraph1e7 5000000".format(cwd))
-if RUN_UARCH: gbwtRunner.runVtuneUarch("{}/Gbwt/bin/gbwt.prof $KERNEL_DATA/Gbwt/McFullGraph1e7 5000000".format(cwd))
-if RUN_CACHE: gbwtRunner.runVtuneCache("{}/Gbwt/bin/gbwt.prof $KERNEL_DATA/Gbwt/McFullGraph1e7 5000000".format(cwd))
-pinRuns.append(lambda:gbwtRunner.runPinInstrCount("{}/Gbwt/bin/gbwt.prof $KERNEL_DATA/Gbwt/McFullGraph1e7 500000".format(cwd)))
+if RUN_VANILLA : gbwtRunner.runVanillaApp("{}/Gbwt/bin/gbwt.prof $KERNEL_DATA/Gbwt/Gbwt1e7".format(cwd))
+if RUN_UARCH: gbwtRunner.runVtuneUarch("{}/Gbwt/bin/gbwt.prof $KERNEL_DATA/Gbwt/Gbwt1e7".format(cwd))
+if RUN_CACHE: gbwtRunner.runVtuneCache("{}/Gbwt/bin/gbwt.prof $KERNEL_DATA/Gbwt/Gbwt1e7".format(cwd))
+pinRuns.append(lambda:gbwtRunner.runPinInstrCount("{}/Gbwt/bin/gbwt.prof $KERNEL_DATA/Gbwt/Gbwt1e7".format(cwd)))
 
 #load time: 9258875us
 #kernel time: 29103560us
 #write time: 669us
 #Don't expect runtime to scale linearly. Some iterations are MUCH slower than others.
 gwfaRunner = Runner("AllRunsOut/Gwfa")
-gwfaRunner.runVanillaApp("{}/Gwfa/bin/gwfa.prof $KERNEL_DATA/Gwfa/McChm13 60".format(cwd))
-if RUN_UARCH: gwfaRunner.runVtuneUarch("{}/Gwfa/bin/gwfa.prof $KERNEL_DATA/Gwfa/McChm13 60".format(cwd))
-if RUN_CACHE: gwfaRunner.runVtuneCache("{}/Gwfa/bin/gwfa.prof $KERNEL_DATA/Gwfa/McChm13 60".format(cwd))
-pinRuns.append(lambda:gwfaRunner.runPinInstrCount("{}/Gwfa/bin/gwfa.prof $KERNEL_DATA/Gwfa/McChm13 30".format(cwd)))
+if RUN_VANILLA : gwfaRunner.runVanillaApp("{}/Gwfa/bin/gwfa.prof $KERNEL_DATA/Gwfa/GwfaChrom".format(cwd))
+if RUN_UARCH: gwfaRunner.runVtuneUarch("{}/Gwfa/bin/gwfa.prof $KERNEL_DATA/Gwfa/GwfaChrom".format(cwd))
+if RUN_CACHE: gwfaRunner.runVtuneCache("{}/Gwfa/bin/gwfa.prof $KERNEL_DATA/Gwfa/GwfaChrom".format(cwd))
+pinRuns.append(lambda:gwfaRunner.runPinInstrCount("{}/Gwfa/bin/gwfa.prof $KERNEL_DATA/Gwfa/GwfaChrom 60".format(cwd)))
+
+#load time: 9258875us
+#kernel time: 29103560us
+#write time: 669us
+#Don't expect runtime to scale linearly. Some iterations are MUCH slower than others.
+gwfaRunnerLr = Runner("AllRunsOut/GwfaLr")
+if RUN_VANILLA : gwfaRunnerLr.runVanillaApp("{}/Gwfa/bin/gwfa.prof $KERNEL_DATA/Gwfa/GwfaLr".format(cwd))
+if RUN_UARCH: gwfaRunnerLr.runVtuneUarch("{}/Gwfa/bin/gwfa.prof $KERNEL_DATA/Gwfa/GwfaLr".format(cwd))
+if RUN_CACHE: gwfaRunnerLr.runVtuneCache("{}/Gwfa/bin/gwfa.prof $KERNEL_DATA/Gwfa/GwfaLr".format(cwd))
+pinRuns.append(lambda:gwfaRunnerLr.runPinInstrCount("{}/Gwfa/bin/gwfa.prof $KERNEL_DATA/Gwfa/GwfaLr 1000".format(cwd)))
 
 #we run these in parallel because they are very slow, and their results aren't
 #affected if the machine is running other processes
