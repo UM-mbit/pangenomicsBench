e9e840ce837f1eeee9e4eba5c9ce9589df382468
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2025-06-11 11:43:01 -0400

    added script for analyzing nsight compute profile results

---
+++ Pgsgd_gpu/analyze_profile_results.py
@@ -0,0 +1,35 @@
+import ncu_report
+import numpy as np
+
+report = ncu_report.load_report("profile_results.ncu-rep")
+report = report[0]
+
+d = np.zeros((30,3))
+
+for i in range(len(report)):
+    occupancy = report[i]['sm__warps_active.avg.pct_of_peak_sustained_active'].value()
+    warp_utilization = report[i]['smsp__thread_inst_executed_pred_on_per_inst_executed.ratio'].value() #/ 32.0
+    mem_throughput = report[i]['gpu__compute_memory_throughput.avg.pct_of_peak_sustained_elapsed'].value()
+
+    d[i] = [occupancy, warp_utilization, mem_throughput]
+
+print("Occupancy, Warp Utilization, Memory Throughput")
+print(d)
+
+mean_glob = np.mean(d, axis=0)
+print("\nGlobal:")
+print("Mean Occupancy:", mean_glob[0])
+print("Mean Warp Utilization:", mean_glob[1])
+print("Mean Memory Throughput:", mean_glob[2])
+
+mean_block0 = np.mean(d[:15], axis=0)
+print("\nIteration 1-15:")
+print("Mean Occupancy:", mean_block0[0])
+print("Mean Warp Utilization:", mean_block0[1])
+print("Mean Memory Throughput:", mean_block0[2])
+
+mean_block1 = np.mean(d[15:], axis=0)
+print("\nIteration 16-30:")
+print("Mean Occupancy:", mean_block1[0])
+print("Mean Warp Utilization:", mean_block1[1])
+print("Mean Memory Throughput:", mean_block1[2])
--- Pgsgd_gpu/pgsgd_gpu_profiling.sh
+++ Pgsgd_gpu/pgsgd_gpu_profiling.sh
@@ -4,8 +4,8 @@ if [[ -v KERNEL_DATA ]]; then
     # CPU
     #odgi/bin/odgi layout -i $KERNEL_DATA/Pgsgd/chr20_smooth.og -o chr20_smooth.lay -t 32 -P 2>&1 | tee pgsgd_cpu_profiling.log
     # GPU
-    ncu --target-processes all --set full --kernel-name gpu_layout_kernel --launch-skip 13 --launch-count 4 -o profile_results odgi/bin/odgi layout -i $KERNEL_DATA/Pgsgd/chr20_smooth.og -o chr20_smooth_gpu.lay -t 32 --gpu -P
-    ncu --import profile_results.ncu-rep --metrics sm__warps_active.avg.pct_of_peak_sustained_active,smsp__thread_inst_executed_pred_on_per_inst_executed.ratio,gpu__compute_memory_throughput.avg.pct_of_peak_sustained_elapsed
+    ncu --target-processes all --set full --kernel-name gpu_layout_kernel -o profile_results -f odgi/bin/odgi layout -i $KERNEL_DATA/Pgsgd/chr20_smooth.og -o chr20_smooth_gpu.lay -t 32 --gpu -P
+    python analyze_profile_results.py | tee analyze.log
 else
     echo "Environment variable KERNEL_DATA not set!"
 fi
