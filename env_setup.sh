d1c4a93a49a6c694459af26457a30afeae9e2a40
Niklas Schmelzle <niklas.schmelzle@gmail.com>
2025-08-23 01:39:12 -0400

    fix source script

--- env_setup.sh
+++ env_setup.sh
@@ -1,2 +1,2 @@
-export LD_LIBRARY_PATH=${PWD}/deps/seqwish/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=${PWD}/deps/odgi/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${PWD}/Tc/deps/seqwish/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${PWD}/Pgsgd/deps/odgi/lib:$LD_LIBRARY_PATH
